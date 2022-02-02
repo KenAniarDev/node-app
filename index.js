847c5c0125bef264e1f53c18bcdc9f3d5ba5b29d
Nestor Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-02-02 13:48:53 +0800

    add test env

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 3000
+const mongdb_uri = process.env.MONGODB_URI || 'not successful'
+
 
 app.get('/', (req, res) => {
-  res.send(`port: ${port}`)
+  res.send(`port: ${mongdb_uri}`)
 })
 
 app.listen(port, () => {
